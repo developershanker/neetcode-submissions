787fcfecde957b5c19c44cb7f8d44baf3400b46e
Bhawana Shanker <67838758+developershanker@users.noreply.github.com>
2026-04-20 14:33:31 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+
+    isAnagram(s, t) {
+       const getSortedStr = (str)=>{
+        return str?.split("").sort().join("")
+    }
+        const sortedA = getSortedStr(s)
+        const sortedB = getSortedStr(t)
+        return sortedA === sortedB
+    }
+}
